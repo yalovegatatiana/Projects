099abd109ff3f406910fb6dd6ff6f289ffd5630a
yalovegatatiana <yalovegatatiana@gmail.com>
2026-04-28 20:28:38 +0300

    add vector

--- main.cpp
+++ main.cpp
@@ -8,5 +8,10 @@ int main()
    cout << "Hello world!!!" << endl;
    
    vector<std::string> vec{"one", "two"};
+   for(const auto& item : vec)
+   {
+      cout << item << endl;
+   }
+   
    return 0;
 }
