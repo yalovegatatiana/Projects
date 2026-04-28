09db63d1263ebb838fa30ae26d1934d7d4b4e548
yalovegatatiana <yalovegatatiana@gmail.com>
2026-04-28 18:52:28 +0300

    change gitignore file

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
    cout << "Hello world!!!" << endl;
+   
+   vector<std::string> vec{"one", "two"};
    return 0;
 }
